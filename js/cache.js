ff0c84cfa89fa7f6c52dafc431c01b42f3a21fb8
almickle <michaelcdear@gmail.com>
2022-12-03 22:25:22 -0500

    updated header

--- js/cache.js
+++ js/cache.js
@@ -1,12 +1,8 @@
 const headers = {
     "Host": "*",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-us,en;q=0.5",
-    "Accept-Encoding": "gzip,deflate",
-    "Connection": "keep-alive",
     "Origin": "https://almickle.github.io/Practice/",
-    "Access-Control-Request-Method": "POST",
-    "Access-Control-Request-Headers": "X-PINGOTHER, Content-Type"
+    "Access-Control-Request-Method": "HEAD",
+    "Access-Control-Request-Headers": "Content-Length"
 }
 
 fetch("https://api.ncbi.nlm.nih.gov/datasets/v2alpha", {
@@ -14,4 +10,5 @@ fetch("https://api.ncbi.nlm.nih.gov/datasets/v2alpha", {
     headers: headers
 })
 .then((resp) => console.log(resp))
-.catch((err) => console.log(err))
+
+console.log('updated')
